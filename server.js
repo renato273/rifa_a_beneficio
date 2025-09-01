da7db969da2e8a4e405e4e58a50681716764de6b
renato273 <alvarezortegarenato566@gmail.com>
2025-09-01 13:06:30 -0300

    Actualizacion de Puerto

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const compression = require('compression');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 // Configuración para producción
 if (process.env.NODE_ENV === 'production') {
